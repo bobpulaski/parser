960b2985b4040fd7c4bb0a3523e6e82aa44619af
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2022-04-12 15:44:27 +0300

    vodonagrev

--- algoritmselect.php
+++ algoritmselect.php
@@ -1,22 +1,23 @@
 <?php
 
-   
-    function selectParcer($wtf) {
-    
-        switch ($wtf) {
-            case 1:
-                return array('Котлы отопительные|Газовые котлы;', '/CSV/kotl_gaz.csv', '/URLS/01-gaz-kotel.php');
-                break;
-            case 2:
-                return array('Котлы отопительные|Электрические котлы;', '/CSV/kotl-electro.csv', '/URLS/02-electro-kotel.php');
-                break;
 
-            case 3:
-                return array('Колонки газовые;', '/CSV/kolonki-gaz.csv', '/URLS/03-gaz-kolonki.php');
-                break;
+function selectParcer($wtf)
+{
 
-                case 4:
-                    return array('Водонагреватели|Бойлеры косвенного нагрева;', '/CSV/vodonagrev-kosven.csv', '/URLS/04-vodonagrev-kosven.php');
-                    break;
-        }
+    switch ($wtf) {
+        case 1:
+            return array('Котлы отопительные|Газовые котлы;', '/CSV/kotl_gaz.csv', '/URLS/01-gaz-kotel.php');
+            break;
+        case 2:
+            return array('Котлы отопительные|Электрические котлы;', '/CSV/kotl-electro.csv', '/URLS/02-electro-kotel.php');
+            break;
+
+        case 3:
+            return array('Колонки газовые;', '/CSV/kolonki-gaz.csv', '/URLS/03-gaz-kolonki.php');
+            break;
+
+        case 4:
+            return array('Водонагреватели|Бойлеры косвенного нагрева;', '/CSV/vodonagrev-kosven.csv', '/URLS/04-vodonagrev-kosven.php');
+            break;
     }
+}
--- parcer.php
+++ parcer.php
@@ -2,12 +2,13 @@
 
 require_once('vendor/autoload.php');
 require_once('algoritmselect.php');
+
 use DiDom\Document;
 use DiDom\Query;
 
 //МЕНЯЕМ!
 
-    $wtf = 4;
+$wtf = 4;
 
 //*************//
 
@@ -23,11 +24,11 @@ $urls = include(__DIR__ . $select[2]);
 $start = microtime(true);
 
 for ($n = 0; $n <= count($urls) - 1; $n++) {
-    
+
     echo 'Loading... ' . $urls[$n] . PHP_EOL;
     $start_inner = microtime(true);
 
-    
+
     $document = new Document($urls[$n], true);
 
     $name = $document->find('h1')[0]->text();
@@ -36,40 +37,33 @@ for ($n = 0; $n <= count($urls) - 1; $n++) {
     $image = $document->find('a[class="gallery-picture-link link-text-decoration-none"]')[0]->getAttribute('href');
     $model = $document->find('meta[itemprop=sku]')[0]->getAttribute('content');
     $meta_description = 'Купить ' . $name . ' в Пензе с бесплатной доставкой по России. Выгодная цена, оплата при получении, скидки и акции, гарантия. Описание, фото и отзывы на товар.';
-            
 
-    $specifications = include(__DIR__.'/specifications_array.php');
+
+    $specifications = include(__DIR__ . '/specifications_array.php');
 
     $steps = '';
     for ($i = 0; $i <= count($specifications) - 1; $i++) {
-        if (isset($document->find("//div[@class='properties-item-name' and contains(text(), '$specifications[$i]')]", Query::TYPE_XPATH)[0])) 
-        {
-            
+        if (isset($document->find("//div[@class='properties-item-name' and contains(text(), '$specifications[$i]')]", Query::TYPE_XPATH)[0])) {
+
             $step = 'Характеристики|' . trim($document->find("//div[@class='properties-item-name' and contains(text(), '$specifications[$i]')]", Query::TYPE_XPATH)[0]->text()) .
                 '|' .  trim($document->find("//div[@class='properties-item-name' and contains(text(), '$specifications[$i]')]/following::span", Query::TYPE_XPATH)[0]->text());
-                $steps .= trim($step) . PHP_EOL;
+            $steps .= trim($step) . PHP_EOL;
         }
-    } 
+    }
 
     $attributes .= $steps;
-    $all .= ($name . ';' . $price . ';' . $category . $image . ';' . '"' . $attributes . '"' . ';' . ('KSO-' . $model) .';' . $meta_description . PHP_EOL);
+    $all .= ($name . ';' . $price . ';' . $category . $image . ';' . '"' . $attributes . '"' . ';' . ('KSO-' . $model) . ';' . $meta_description . PHP_EOL);
     $attributes = '';
 
     echo ' Parsing ' . round(microtime(true) - $start_inner, 2) . ' секунд' . PHP_EOL;
-    
 }
 
 $headers = '_NAME_;_PRICE_;_CATEGORY_;_IMAGE_;_ATTRIBUTES_;_MODEL_;_META_DESCRIPTION_';
 $summary = $headers . PHP_EOL . $all;
 //$summary = $headers . PHP_EOL . $names . ';' . $price . ';' . $category . $image . ';' . '"' . $attributes . '"' . ';' . 'KSO-TTTT0240' . PHP_EOL;
 
-$file = fopen(__DIR__.$select[1], 'w');
+$file = fopen(__DIR__ . $select[1], 'w');
 fwrite($file, $summary); // Запись в файл
 fclose($file); // Закрытие файла
 
 echo 'Скрипт был выполнен за ' . round(microtime(true) - $start, 2) . ' секунд';
-
-
-
-
-
