6e609a67b317c5d78d9b83df5fedfe0f30043b1f
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-04-24 22:43:26 +0300

    24-04-22-home-phpstorm

--- getlinks.php
+++ getlinks.php
@@ -1,28 +1,27 @@
 <?php
 
-require_once('vendor/autoload.php');
+require_once ('vendor/autoload.php');
+
 use DiDom\Document;
 use DiDom\Query;
 
 $url = 'https://www.pogodavdome73.ru/categories/gazovye-kotly?page=14';
 
 
-while ($document = @file_get_contents($url) !== FALSE) 
-    {
-        $document = new Document($url, true);
-        $i = 0;
-        $links = '';
-        
-        while (isset($document->find('a[class="products-view-picture-link"]')[$i])) 
-            {
-                $link = '"' . $document->find('a[class="products-view-picture-link"]')[$i]->getAttribute('href') . '",' . PHP_EOL;
-                $links .= $link;
-                $i++;
-            }
+while ($document = @file_get_contents ($url) !== FALSE) {
+    $document = new Document($url, true);
+    $i = 0;
+    $links = '';
 
-echo $links;
+    while (isset($document->find ('a[class="products-view-picture-link"]')[$i])) {
+        $link = '"' . $document->find ('a[class="products-view-picture-link"]')[$i]->getAttribute ('href') . '",' . PHP_EOL;
+        $links .= $link;
+        $i++;
     }
-    
+
+    echo $links;
+}
+
 
 
 
